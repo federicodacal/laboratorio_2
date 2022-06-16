39be9ef47620386c906222b5162aa82028fb7ef3
Federico Dacal <federicodacal@gmail.com>
2022-06-16 19:13:26 -0300

    metodo de extension

---
+++ Ejemplos/Clase_20_Metodos_de_Extension/EjemploStringExtension/Consola/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Entidades;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hola Mundo";
+            string b = "Métodos de Extensión";
+            string c = "C#";
+            string d = "C# is a general-purpose, multi-paradigm programming language. C# encompasses static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented and component-oriented programming disciplines";
+
+            Console.WriteLine($"{a}{Environment.NewLine}Tiene {a.ContarCaracteres()} caracteres, {a.ContarVocales()} vocales y {a.ContarPalabras()} palabras");
+            Console.WriteLine();
+            Console.WriteLine($"{b}{Environment.NewLine}Tiene {b.ContarCaracteres()} caracteres, {b.ContarVocales()} vocales y {b.ContarPalabras()} palabras");
+            Console.WriteLine();
+            Console.WriteLine($"{c}{Environment.NewLine}Tiene {c.ContarCaracteres()} caracteres, {c.ContarVocales()} vocales y {c.ContarPalabras()} palabras");
+            Console.WriteLine();
+            Console.WriteLine($"{d}{Environment.NewLine}Tiene {d.ContarCaracteres()} caracteres, {d.ContarVocales()} vocales y {d.ContarPalabras()} palabras");
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Ejemplos/Clase_20_Metodos_de_Extension/EjemploStringExtension/Entidades/StringExtension.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entidades
+{
+    public static class StringExtension
+    {
+        public static int ContarCaracteres(this string cadena)
+        {
+            int totalCaracteres = 0;
+
+            if(cadena is not null)
+            {
+                totalCaracteres = cadena.Trim().Length;
+            }
+            return totalCaracteres;
+        }
+
+        public static int ContarVocales(this string cadena)
+        {
+            int totalVocales = 0;
+            
+            List<char> vocales = new List<char>()
+            { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'á', 'é', 'í', 'ó', 'ú' }; 
+
+
+            if(cadena is not null)
+            {
+                foreach (char item in cadena)
+                {
+                    if (vocales.Contains(item))
+                    {
+                        totalVocales++;
+                    }
+                }
+            }
+            return totalVocales;
+        }
+
+        public static int ContarPalabras(this string cadena)
+        {
+            int totalPalabras = 0;
+
+            if(cadena is not null)
+            {
+                string[] palabras = cadena.Trim().Split(' ');
+
+                totalPalabras = palabras.Length;
+            }
+
+            return totalPalabras;
+        }
+    }
+}
